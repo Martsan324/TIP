468282c52df08590151a63404862b49ce094800f
martsan324@gmail.com <martsan324@gmail.com>
2017-05-02 15:28:48 +0200

    Adding Ozeki library

---
+++ testy/01_BasePBX/PBX/01_BasePBX/BasePBX/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ozeki.Network;
+
+namespace BasePBX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPBX = new BasePBX.MyPBX(NetworkAddressHelper.GetLocalIP().ToString(), 20000, 20500);
+            myPBX.Start();
+
+            Console.ReadLine();
+            myPBX.Stop();
+        }
+    }
+}
